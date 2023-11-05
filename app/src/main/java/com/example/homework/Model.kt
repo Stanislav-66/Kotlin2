be6abad7126396d72fdcbaff2f93626352b76595
Admin <serafimraser@gmail.com>
2023-11-05 08:53:33 +0300

    Homework complete

---
+++ app/src/main/java/com/example/homework/Model.kt
@@ -0,0 +1,31 @@
+package com.example.homework
+
+class Model {
+    fun getSum(a : Int, b : Int) = a + b
+    fun getSumMore(vararg a : Int) = if(a.size >= 2) a.sum() else throw IllegalArgumentException("Неверное количество аргументов")
+
+
+    //Task2
+    fun task2(char : Char = ' ', vararg str : String) : String
+    {
+        return str.joinToString(char.toString())
+    }
+
+    //task4
+    fun task4(f:()->Unit) : Long
+    {
+        val startTime = System.currentTimeMillis()
+        f()
+        val endTime = System.currentTimeMillis()
+        return endTime - startTime
+    }
+
+
+    fun testFunction() {
+        var n = 1000000000
+        while (n != 0)
+        {
+            n--
+        }
+    }
+}
