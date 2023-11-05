0b36ffe9fa81ac24b93c516f6ad2d285bc117f34
Admin <serafimraser@gmail.com>
2023-11-05 09:02:09 +0300

    task2 complete

--- app/src/main/java/com/example/homework/Model.kt
+++ app/src/main/java/com/example/homework/Model.kt
@@ -1,12 +1,13 @@
 package com.example.homework
 
 class Model {
+    //Task1
     fun getSum(a : Int, b : Int) = a + b
     fun getSumMore(vararg a : Int) = if(a.size >= 2) a.sum() else throw IllegalArgumentException("Неверное количество аргументов")
 
 
     //Task2
-    fun task2(char : Char = ' ', vararg str : String) : String
+    fun task2 (char : Char = ' ', vararg str : String) : String
     {
         return str.joinToString(char.toString())
     }
